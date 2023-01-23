de71246ca4978717515f7f94ef3000c690a1dfee
Jamshidbek <jamshidbek1805@gmail.com>
2023-01-23 14:05:10 +0500

    added answer of power_of_tree

---
+++ power_of_three/main.go
@@ -0,0 +1,10 @@
+func isPowerOfThree(n int) bool {
+	var number int = 0
+	for i := 0; number < n; i++ {
+		if int(math.Pow(3, float64(i))) == n {
+			return true
+		}
+		number = int(math.Pow(3, float64(i)))
+	}
+	return false
+}
