b2e0d8f549563a9cd399aec9ff3a914db951a277
Jamshidbek <jamshidbek1805@gmail.com>
2023-01-23 14:25:07 +0500

    added answer missing_number question

---
+++ missing_number/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "sort"
+
+func missingNumber(nums []int) int {
+	sort.Ints(nums)
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != i {
+			return i
+		}
+
+	}
+	return len(nums)
+}
